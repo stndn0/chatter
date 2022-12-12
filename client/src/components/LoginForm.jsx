f629380943425f3616bec4f6d82f38f0aa0e0830
st <49390281+stndn0@users.noreply.github.com>
2022-12-12 13:42:53 +1100

    client can make post requests using fetch. server will respond with json

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,29 +1,73 @@
 import React from 'react';
 import './LoginForm.css';
+import { useState } from 'react'
 
 // Login form contents is sent to the back end
 const postRoute = "http://localhost:5000/auth/login"
 
+
 export function LoginForm() {
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  // Gets the values of the input fields and updates the state.
+  const handleUsernameInput = event => {
+    setUsername(event.target.value)
+  }
+  const handlePasswordInput = event => {
+    setPassword(event.target.value)
+  }
+
+  // Post username and password to server and handle response from server.
+  const loginToServer = () => {
+    // Stop the page from reloading when the user clicks the Login button.
+    // See: https://stackoverflow.com/questions/63880605/react-js-how-to-prevent-page-reload-once-click-on-the-link-right-now-the-whole
+    event.preventDefault();
+
+    console.log("SENDING TO SERVER")
+
+    sendToServer(postRoute, { username, password })
+      .then((data) => {
+        console.log(data);
+      })
+  };
+
+  async function sendToServer(url, data) {
+    const options = {
+      method: 'POST',
+      // Headers consist of meta data. We're telling the server that we're sending a JSON.
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+
+    const response = await fetch(url, options);
+    return response.json();
+  }
+
+
+
   return (
     <div id="login-form-container">
       <h1>Login</h1>
 
-      <form id="form" action={postRoute} method="POST">
+      <form id="form">
         <div id="user-details">
           <label>Username</label>
-          <input name="username" type="text" id="username" className="textbox" placeholder="Type your username"></input>
+          <input name="username" type="text" id="username" className="textbox" placeholder="Type your username" onChange={handleUsernameInput}></input>
           <label>Password</label>
-          <input name="password" type="text" id="password" className="textbox" placeholder="Type your password"></input>
+          <input name="password" type="text" id="password" className="textbox" placeholder="Type your password" onChange={handlePasswordInput}></input>
         </div>
 
 
         <div id="buttons-container">
-          <button name="login" id="button-login01">LOGIN</button>
-          <button id="button-login01">LOGIN (TEST)</button>
+          <button name="login" id="button-login01" onClick={() => loginToServer()} >LOGIN</button>
+          <button id="button-login02">LOGIN (TEST)</button>
         </div>
 
         <p></p>
+        {/* {console.log(password)} */}
       </form>
     </div>
   )
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,21 @@
 exports.verifyInput = (req, res) => {
     try {
+        console.log("GOT POST")
+        console.log(req.body)
+
         const username = req.body.username;
         const password = req.body.password;
 
-        if (username.length < 3 || password.length < 3) {
-            res.send("The server recieved invalid input.")
+        if (username === null || password === null) {
+            res.json({"message": "Server recieved invalid input."})
+        }
+        else if (username.length < 3 || password.length < 3) {
+            res.json({"message": "Server recieved invalid input."})
         }
         else {
             // Check the database to see if the username and password exist...
-            res.send("User: " + username + " Password: " + password)
+            res.json({"username": req.body.username, "password": req.body.password})
+            // res.send("User: " + username + " Password: " + password)
         }
     } catch (error) {
         console.log("Error when sending data from server (verifyInput)")
