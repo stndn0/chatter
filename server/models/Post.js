af883817490898349661709fe2ee6f3c03967f11
st <49390281+stndn0@users.noreply.github.com>
2022-12-20 13:10:47 +1100

    updated models

---
+++ server/models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;  
+
+const postSchema = new Schema({
+    userid: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    post: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Post', postSchema)
--- server/models/User.js
+++ server/models/User.js
@@ -22,6 +22,10 @@ const userSchema = new Schema({
         type: String,
         required: false
     },
+    userid: {
+        type: String,
+        required: true
+    },
     refreshToken: String
 })
 
