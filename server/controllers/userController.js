cbef27831ba809462ab538fa05760eb654628bf0
st <49390281+stndn0@users.noreply.github.com>
2022-12-20 14:08:59 +1100

    improve database model for sending new tweet

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,8 @@
 // Database models
 const User = require('../models/User');
 const Post = require('../models/Post');
-
+// Used to generate random string
+const crypto = require('crypto')        
 
 exports.updateUserBio = async (req, res) => {
     try {
@@ -18,10 +19,25 @@ exports.updateUserBio = async (req, res) => {
 
 exports.newPost = async (req, res) => {
     try {
-        console.log(req.body);
+        let postid = crypto.randomBytes(5).toString('hex');
+        let date = new Date().toLocaleDateString();
+        let time = new Date().toLocaleTimeString('en-US', {hour12: false})
+
+
+        const data = {
+            userid: req.body.userid, 
+            postid: postid,
+            post: req.body.post,
+            likes: 0,
+            reposts: 0,
+            date: date,
+            time: time
+        }
+
+        console.log(data)
 
         // To insert a document with *Mongoose* we use the create method.
-        const update = await Post.create( {userid: req.body.userid, post: req.body.post} )
+        // const update = await Post.create( {userid: req.body.userid, post: req.body.post} )
 
     } catch (error) {
         console.log(error)
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;  
+const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
     userid: {
         type: String,
         required: true
     },
+    postid: {
+        type: String,
+        required: true
+    },
     post: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: String,
+        required: true
+    },
+    reposts: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
         type: String,
         required: true
     }
