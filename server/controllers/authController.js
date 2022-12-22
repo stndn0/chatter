5fda8ff6c4f85672561af6bebbbb107106055d7a
st <49390281+stndn0@users.noreply.github.com>
2022-12-22 13:23:18 +1100

    wrap auth middleware in trycatch block

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,14 +38,14 @@ exports.veryifyLoginInput = async (req, res) => {
 
                     // Create a JSON Access Token and a JSON Refresh Token.
                     // Serialize the user with a name and a secret key.
-                    const accessToken = jwt.sign({username: username}, process.env.ACCESS_TOKEN_SECRET);
+                    const accessToken = jwt.sign({ username: username }, process.env.ACCESS_TOKEN_SECRET);
                     // const accessToken = jwt.sign({username: username}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
                     const refreshToken = jwt.sign(username, process.env.REFRESH_TOKEN_SECRET);
-                    res.json({ 
-                        "Server Response": "Successful login", 
-                        "accessToken": accessToken, 
-                        "refreshToken": refreshToken, 
-                        "username": username ,
+                    res.json({
+                        "Server Response": "Successful login",
+                        "accessToken": accessToken,
+                        "refreshToken": refreshToken,
+                        "username": username,
                         "bio": bio,
                         "userid": userid,
                     });
@@ -69,36 +69,41 @@ exports.veryifyLoginInput = async (req, res) => {
 // User token must be authenticated before a user is permitted to do anything
 // that requires authorization (e.g view timeline, login...)
 exports.authenticateToken = (req, res, next) => {
-    // Parse incoming request from client. Read the header and verify the token.
-    const authHeader = req.headers['authorization'];
-    const token = authHeader.split(' ')[1];
+    try {
+        // Parse incoming request from client. Read the header and verify the token.
+        const authHeader = req.headers['authorization'];
+        const token = authHeader.split(' ')[1];
 
-    if (token == 'null') {
-        console.log("Auth error. Invalid token.")
-        return res.json({ "Server Response": "Unauthorized request." });
+        if (token == 'null') {
+            console.log("Auth error. Invalid token.")
+            return res.json({ "Server Response": "Unauthorized request." });
+        }
+
+        // Check token to see if it's valid.
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
+            // Remember that the 'username' variable has already been serialized 
+            // in memory back when the login request was handled.
+
+            // Case: server recieved an access token that is expired. 
+            if (err) {
+                // Ask client to send their refresh token so that the server can verify it.
+                console.log("Token no longer valid. Requesting refresh token.")
+                return res.json({ "Server Response": "Unauthorized. Your token is no longer valid." });
+            }
+            // Valid token
+            else {
+                // TODO - WATCH THIS (13:30) FOR RETURNING USER SPECIFIC DATA
+                // https://www.youtube.com/watch?v=mbsmsi7l3r4&t=30s
+
+                // console.log("Authorization successful for", username);
+                // // Move on from the middleware
+                // req.username = username;
+                next();
+                return;
+            }
+        })
+    } catch (error) {
+        return res.json({ "Server Response": "Unauthorized." });
     }
 
-    // Check token to see if it's valid.
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
-        // Remember that the 'username' variable has already been serialized 
-        // in memory back when the login request was handled.
-
-        // Case: server recieved an access token that is expired. 
-        if (err) {
-            // Ask client to send their refresh token so that the server can verify it.
-            console.log("Token no longer valid. Requesting refresh token.")
-            return res.json({ "Server Response": "Unauthorized. Your token is no longer valid." });
-        }
-        // Valid token
-        else {
-            // TODO - WATCH THIS (13:30) FOR RETURNING USER SPECIFIC DATA
-            // https://www.youtube.com/watch?v=mbsmsi7l3r4&t=30s
-
-            // console.log("Authorization successful for", username);
-            // // Move on from the middleware
-            // req.username = username;
-            next();
-            return;
-        }
-    })
 }
