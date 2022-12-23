aef7108e32ec8d5fa891406a039b305c510fa743
st <49390281+stndn0@users.noreply.github.com>
2022-12-23 18:10:08 +1100

    change information displayed on post and improve css

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,6 +4,8 @@ import './Post.css'
 export default function Post(data) {
     console.log("Inside Post Component")
     const postData = data.data;
+    const date = new Date(postData.date).toLocaleDateString();
+    const time = new Date(postData.date).toLocaleTimeString('en-US');
 
     return (
         <div id='post-container'>
@@ -13,16 +15,19 @@ export default function Post(data) {
                 <div id="post-content">
                     <div className="post-username">{postData.username}</div>
 
-                    <div className="post-body">{postData.post} {postData.date}  {postData.time}</div>
+                    <div className="post-body">{postData.post}</div>
 
-                    {/* <div className="post-data">{postData.date}  {postData.time}</div> */}
 
-                    <div className="post-social-row">
-                        <div id='reply'>🗨️</div>
-                        <div id='boost'>🔁</div>
-                        <div id='like'>💗</div>
+
+                    <div className="post-bottom-row">
+                        {/* <div id='boost'>🔁</div> */}
+                        <div id="post-social-items">
+                            <div id='like'>💗</div>
+                            <div id='reply'>🗨️</div>
+                        </div>
+                        <div className="post-time"> {date} at {time}</div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
